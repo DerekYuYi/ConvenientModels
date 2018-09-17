c7a7e7a5efc5b287722f2a9f51b57eeb234c4e88
Yuyi <derekyuyi@foxmail.com>
2018-09-17 18:48:06 +0800

    Adds VisualEffectViewPage.

--- Summer/ViewController.swift
+++ Summer/ViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController, CAAnimationDelegate {
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var gradientLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let animView = GradientAnimationView(frame: CGRect(x: 16, y: self.view.bounds.height-64, width: self.view.bounds.width - 16*2, height: 44))
+        animView.backgroundColor = .red
+        animView.text = "Slide to reveal"
+        view.addSubview(animView)
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
